fe879375566a54c14a82efd2c295d6f7d4d83784
SUraj Negi <er.surajnegi@gmail.com>
2017-07-18 01:30:06 +0530

    fdfd

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -34,7 +34,7 @@ export class TasksComponent {
         this.getTasks();
         return;
       }
-      this.userDetails = null;
+      this.userDetails = new Object();
     })
   }
   getTasks() {
